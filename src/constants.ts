fd5a2477e55f3ca1f50f1e4278e1cef2460e6fd3
Rouvin <rouvinerh@gmail.com>
2025-06-01 20:47:01 +0800

    tidy up images and files

--- src/constants.ts
+++ src/constants.ts
@@ -17,7 +17,7 @@ export const ABOUT_PHRASES = [
     "hacking for good."
 ];
 
-export const ABOUT_DESCRIPTION = "Hello, I'm Rouvin, an InfoSec student from the National University of Singapore specialising in offensive security.";
+export const ABOUT_DESCRIPTION = "Hello! I'm Rouvin, an InfoSec student from the National University of Singapore specialising in Offensive Security. ";
 
 export const SOCIAL_LINKS = [
     {
@@ -41,31 +41,31 @@ export const CERTIFICATIONS = [
     {
         href: "https://credentials.offsec.com/8cf53528-7f84-458e-b035-9109ba5af955",
         tooltip: "OffSec Exploit Developer",
-        imageSrc: "osed-logo.svg",
+        imageSrc: "assets/logos/osed-logo.svg",
         alt: "OSED"
     },
     {
         href: "https://credentials.offsec.com/e0767f9c-9826-4eac-806b-d63258d28256",
         tooltip: "OffSec Web Expert",
-        imageSrc: "oswe-logo.svg",
+        imageSrc: "assets/logos/oswe-logo.svg",
         alt: "OSWE"
     },
     {
         href: "https://www.credential.net/6d698072-51f3-43f7-83ac-27117c629a82",
         tooltip: "Certified Red Team Expert",
-        imageSrc: "crte-logo.webp",
+        imageSrc: "assets/logos/crte-logo.webp",
         alt: "CRTE"
     },
     {
         href: "https://eu.badgr.com/public/assertions/Ie7VP__qSp-9gAxdwyB1rw?identity__email=rouvinerh@gmail.com",
         tooltip: "Certified Red Team Operator",
-        imageSrc: "crto-logo.png",
+        imageSrc: "assets/logos/crto-logo.png",
         alt: "CRTO"
     },
     {
         href: "https://www.credential.net/e1d80a61-7960-4164-ba82-bdba4292d50b#acc.nQho8jM2",
         tooltip: "OffSec Certified Professional",
-        imageSrc: "oscp-logo.svg",
+        imageSrc: "assets/logos/oscp-logo.svg",
         alt: "OSCP"
     }
 ];
@@ -78,7 +78,7 @@ export const EDUCATION_DATA = {
     deansList: {
         href: "https://credentials.nus.edu.sg/fdfc9ff6-b90c-4e0c-a705-c6fc0ad6b678",
         tooltip: "Dean's List AY23/24 Semester 2",
-        imageSrc: "dean-list-logo.png",
+        imageSrc: "assets/logos/dean-list-logo.png",
         alt: "DeanList"
     }
 };
@@ -89,35 +89,35 @@ export const EXPERIENCE_DATA = [
         company: 'PayPal',
         period: 'May 2025 — Present',
         description: "Triaging bug reports and pentesting under PayPal's Offensive Security team.",
-        logoSrc: 'paypal-logo.png',
+        logoSrc: 'assets/logos/paypal-logo.png',
     },
     {
         role: 'Security Researcher',
         company: 'Synack Red Team',
         period: 'May 2025 — Present',
         description: "Pentesting web and host targets on a freelance basis.",
-        logoSrc: 'synack.webp',
+        logoSrc: 'assets/logos/synack.webp',
     },
     {
         role: 'Cyber Instructor',
         company: 'DART',
         period: 'Mar 2024 — Present',
         description: 'Teaching cybersecurity to students as part of Cyber Youth programme.',
-        logoSrc: 'dart-logo.jpg'
+        logoSrc: 'assets/logos/dart-logo.jpg'
     },
     {
         role: 'Security Engineer Intern',
         company: 'Ascenda Loyalty',
         period: 'May 2024 - Aug 2024',
         description: 'Carried out white-box web application pentesting, identifying 20+ security flaws, along with custom DAST tool development.',
-        logoSrc: 'ascenda-logo.png',
+        logoSrc: 'assets/logos/ascenda-logo.png',
     },
     {
         role: 'Attack Simulation Intern',
         company: 'Cyber Security Agency of Singapore',
         period: 'May 2023 - Aug 2023',
         description: 'Created Caldera adversary profiles and built an Active Directory environment using Vagrant for running profiles.',
-        logoSrc: 'csa-logo.jpg',
+        logoSrc: 'assets/logos/csa-logo.jpg',
     },
 ];
 
@@ -125,38 +125,38 @@ export const PROJECTS_DATA = [
     {
         title: '📚 Security Journal',
         description: 'Writing security blogs, and all my security-related adventures in a GitBook.',
-        imageSrc: 'gitbook.png',
+        imageSrc: 'assets/img/gitbook.png',
         link: 'https://rouvin.gitbook.io',
     },
     {
         title: '🐛 Bug Bounties',
         description: 'Hunting and reporting bugs on bug bounties VDPs when I am free (and bored).',
-        imageSrc: 'xss.png',
+        imageSrc: 'assets/img/xss.png',
         link: '#',
         isSpecial: true
     },
     {
         title: '💀 Malware Development',
         description: 'Learning evasion techniques and various methods of making calc.exe run with Maldev Academy.',
-        imageSrc: 'malwaredev.png',
+        imageSrc: 'assets/img/malwaredev.png',
         link: 'https://github.com/rouvinerh/Malware-Tech',
     },
     {
         title: '🖥️ Boot2Root',
         description: 'Rooted 264 machines on HTB and 99 machines on Proving Grounds and still going!',
-        imageSrc: 'boot2root.png',
+        imageSrc: 'assets/img/boot2root.png',
         link: 'https://app.hackthebox.com/profile/814999',
     },
 ];
 
 export const BUG_POPUP_DATA = {
     caption: "The funniest thing I own:",
-    imageSrc: "best_tshirt.jpg",
+    imageSrc: "assets/img/best_tshirt.jpg",
     imageAlt: "Bug Hunter Mode"
 };
 
 export const RESUME_DATA = {
-    pdfFile: '/RouvinErh_CV.pdf',
+    pdfFile: '/assets/RouvinErh_CV.pdf',
     downloadFilename: 'RouvinErh_CV.pdf',
     defaultWidth: 793
 };
